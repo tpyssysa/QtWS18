a948e340d04236a5afe43d6d3877cdf7a4a9e7eb
Tino Pyssysalo <tino.pyssysalo@qt.io>
2018-10-26 15:38:34 +0300

    instrument cluster cplusplus project

---
+++ instrument_cluster/main.cpp
@@ -0,0 +1,20 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.addImportPath("qrc:/ClusterTutorial/assets/");
+    engine.addImportPath("qrc:/ClusterTutorial/backend/");
+    engine.addImportPath("qrc:/ClusterTutorial/imports/");
+
+    engine.load(QUrl(QStringLiteral("qrc:/ClusterTutorial/ClusterTutorial.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
